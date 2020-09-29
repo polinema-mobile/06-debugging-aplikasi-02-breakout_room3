ee5ea48dc6281391e8d36ce7506dea0b377fed9c
altalarikyusuf98 <altalarik@gmail.com>
2020-09-29 10:17:15 +0700

    Update Test Button Operation

---
+++ app/src/androidTest/java/com/example/new_sample/TestButtonOperation.java
@@ -0,0 +1,111 @@
+package com.example.new_sample;
+
+import androidx.test.filters.LargeTest;
+import androidx.test.rule.ActivityTestRule;
+import androidx.test.runner.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class TestButtonOperation {
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void tesKlikTambah(){
+
+        onView(withId(R.id.button9)).perform(click());
+        onView(withId(R.id.button_add)).perform(click());
+        onView(withId(R.id.button1)).perform(click());
+        onView(withId(R.id.button_equal)).perform(click());
+        onView(withId(R.id.output)).check(matches(withText("10")));
+
+    }
+    @Test
+    public void tesKlikKurang(){
+
+        onView(withId(R.id.button7)).perform(click());
+        onView(withId(R.id.button_sub)).perform(click());
+        onView(withId(R.id.button1)).perform(click());
+        onView(withId(R.id.button_equal)).perform(click());
+        onView(withId(R.id.output)).check(matches(withText("6")));
+
+    }
+
+    @Test
+    public void tesKlikKali(){
+
+        onView(withId(R.id.button6)).perform(click());
+        onView(withId(R.id.button_multi)).perform(click());
+        onView(withId(R.id.button2)).perform(click());
+        onView(withId(R.id.button_equal)).perform(click());
+        onView(withId(R.id.output)).check(matches(withText("12")));
+    }
+
+    @Test
+    public void tesKlikBagi(){
+
+        onView(withId(R.id.button4)).perform(click());
+        onView(withId(R.id.button_divide)).perform(click());
+        onView(withId(R.id.button2)).perform(click());
+        onView(withId(R.id.button_equal)).perform(click());
+        onView(withId(R.id.output)).check(matches(withText("2")));
+    }
+
+    @Test
+    public void tesKlikPara1(){
+
+        onView(withId(R.id.button6)).perform(click());
+        onView(withId(R.id.button_para1)).perform(click());
+        onView(withId(R.id.button4)).perform(click());
+        onView(withId(R.id.button_equal)).perform(click());
+        onView(withId(R.id.output)).check(matches(withText("2")));
+
+    }
+
+    @Test
+    public void tesKlikPara2(){
+
+        onView(withId(R.id.button9)).perform(click());
+        onView(withId(R.id.button_para2)).perform(click());
+        onView(withId(R.id.output)).check(matches(withText("-9")));
+
+    }
+
+    @Test
+    public void tesKlikHapus(){
+
+        onView(withId(R.id.button5)).perform(click());
+        onView(withId(R.id.button_clear)).perform(click());
+        onView(withId(R.id.button4)).perform(click());
+        onView(withId(R.id.button_add)).perform(click());
+        onView(withId(R.id.button3)).perform(click());
+        onView(withId(R.id.button_equal)).perform(click());
+        onView(withId(R.id.output)).check(matches(withText("7")));
+    }
+
+    @Test
+    public void tesKlikKoma(){
+
+        onView(withId(R.id.button2)).perform(click());
+        onView(withId(R.id.button_dot)).perform(click());
+        onView(withId(R.id.button2)).perform(click());
+        onView(withId(R.id.button_add)).perform(click());
+        onView(withId(R.id.button3)).perform(click());
+        onView(withId(R.id.button_dot)).perform(click());
+        onView(withId(R.id.button5)).perform(click());
+        onView(withId(R.id.button_equal)).perform(click());
+        onView(withId(R.id.output)).check(matches(withText("5.7")));
+    }
+
+}
+
